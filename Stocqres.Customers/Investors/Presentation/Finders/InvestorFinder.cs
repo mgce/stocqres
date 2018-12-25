f47730ed36e4988d202b902a275efe83c8cfef57
Mateusz Gajda <matgajda@outlook.com>
2018-12-25 11:44:57 +0100

    Added investorId and walletId to token claims

---
+++ Stocqres.Customers/Investors/Presentation/Finders/InvestorFinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Stocqres.Core.Queries;
+using Stocqres.Customers.Api.Investors.Presentation;
+using Stocqres.Customers.Api.Investors.Presentation.Queries;
+using Stocqres.Customers.Api.Investors.Presentation.Results;
+using Stocqres.Infrastructure.Projections;
+
+namespace Stocqres.Customers.Investors.Presentation.Finders
+{
+    public class InvestorFinder : IQueryHandler<GetInvestorByUserIdQuery, GetInvestorByUserIdResult>
+    {
+        private readonly IProjectionReader _projectionReader;
+
+        public InvestorFinder(IProjectionReader projectionReader)
+        {
+            _projectionReader = projectionReader;
+        }
+        public async Task<GetInvestorByUserIdResult> HandleAsync(GetInvestorByUserIdQuery query)
+        {
+            var investor = await _projectionReader.GetAsync<InvestorProjection>(i => i.UserId == query.UserId);
+            return new GetInvestorByUserIdResult(investor.Id, investor.WalletId);
+        }
+    }
+}
--- Stocqres.Identity/Application/Services/TokenService.cs
+++ Stocqres.Identity/Application/Services/TokenService.cs
@@ -1,7 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Stocqres.Core.Authentication;
 using Stocqres.Core.Exceptions;
+using Stocqres.Core.Queries;
+using Stocqres.Customers.Api.Investors.Presentation.Queries;
+using Stocqres.Customers.Api.Investors.Presentation.Results;
 using Stocqres.Identity.Domain;
 using Stocqres.Identity.Repositories;
 
@@ -13,16 +17,19 @@ namespace Stocqres.Identity.Application.Services
         private readonly IPasswordHasher<User> _passwordHasher;
         private readonly IJwtHandler _jwtHandler;
         private readonly IRefreshTokenRepository _refreshTokenRepository;
+        private readonly IQueryBus _queryBus;
 
         public TokenService(IUserRepository userRepository, 
             IPasswordHasher<User> passwordHasher, 
             IJwtHandler jwtHandler, 
-            IRefreshTokenRepository refreshTokenRepository)
+            IRefreshTokenRepository refreshTokenRepository,
+            IQueryBus queryBus)
         {
             _userRepository = userRepository;
             _passwordHasher = passwordHasher;
             _jwtHandler = jwtHandler;
             _refreshTokenRepository = refreshTokenRepository;
+            _queryBus = queryBus;
         }
 
         public async Task<JsonWebToken> SignInAsync(string username, string password)
@@ -31,11 +38,23 @@ namespace Stocqres.Identity.Application.Services
             if(user == null)
                 throw new StocqresException("UserCodes does not exist.");
 
+            var investor =
+                await _queryBus.Send<GetInvestorByUserIdQuery, GetInvestorByUserIdResult>(
+                    new GetInvestorByUserIdQuery(user.Id));
+
+            var claimsDictionary = new Dictionary<string, string>();
+
+            if (investor != null)
+            {
+                claimsDictionary.Add("investorId", investor.InvestorId.ToString());
+                claimsDictionary.Add("walletId", investor.WalletId.ToString());
+            }
+
             var result = _passwordHasher.VerifyHashedPassword(user, user.Password, password);
             if (result == PasswordVerificationResult.Failed)
                 throw new StocqresException("Username or password is incorrect.");
 
-            var jwt = _jwtHandler.CreateToken(user.Id.ToString());
+            var jwt = _jwtHandler.CreateToken(user.Id.ToString(), claimsDictionary);
 
             var refreshToken = new RefreshToken(user, _passwordHasher);
             await _refreshTokenRepository.CreateAsync(refreshToken);
